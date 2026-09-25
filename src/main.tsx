0b935881822c086885db1050be36f529a11bb67a
Yuma Ariga <yariga@purdue.edu>
2026-09-25 16:59:39 -0400

    Implement routing with BrowserRouter and Routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./routes/home";
+import ProjectsPage from "./routes/projects";
+import EducationPage from "./routes/education";
+import ContactPage from "./routes/contact";
+
 import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/education" element={<EducationPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
