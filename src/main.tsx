f11a5d0cc400c901a01f4d4b2e842037031a01f6
Yuma Ariga <yariga@purdue.edu>
2026-09-25 17:05:26 -0400

    Fix routing with TanStack Router for GitHub Pages subdirectory

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { RouterProvider, createRouter, RootRoute, Route } from "@tanstack/react-router";
 
 import Home from "./routes/home";
 import ProjectsPage from "./routes/projects";
@@ -9,15 +9,53 @@ import ContactPage from "./routes/contact";
 
 import "./style.css";
 
+// Create root route
+const rootRoute = new RootRoute({
+  component: ({ children }) => <>{children}</>,
+});
+
+// Create routes
+const homeRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "/",
+  component: Home,
+});
+
+const projectsRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "/projects",
+  component: ProjectsPage,
+});
+
+const educationRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "/education",
+  component: EducationPage,
+});
+
+const contactRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "/contact",
+  component: ContactPage,
+});
+
+// Create route tree
+const routeTree = rootRoute.addChildren([homeRoute, projectsRoute, educationRoute, contactRoute]);
+
+// Create router
+const router = createRouter({
+  routeTree,
+  basepath: "/my_portfolio",
+});
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<ProjectsPage />} />
-        <Route path="/education" element={<EducationPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
